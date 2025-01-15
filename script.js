d0d0312273d7c4a2057884181b634b129727660d
Erick Silva <erick04develope@gmail.com>
2025-01-15 19:15:43 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-// Preenchendo dados dinâmicos (Visão Geral do Estoque)
-document.addEventListener('DOMContentLoaded', function() {
-    const totalProdutos = 150;
-    const alertasEstoqueBaixo = 5;
-
-    // Preenchendo os dados na página
-    document.querySelector('.total-produtos').textContent = totalProdutos;
-    document.querySelector('.alertas-estoque-baixo').textContent = alertasEstoqueBaixo;
-});
